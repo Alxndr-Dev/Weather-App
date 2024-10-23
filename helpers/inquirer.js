a453ace90ba6823951404d53ae1e28979817aa79
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-22 23:55:18 -0400

    Adding dependencies

---
+++ helpers/inquirer.js
@@ -0,0 +1,199 @@
+const inquirer = require('inquirer');
+require('colors');
+
+//Opciones del menu
+const preguntas =[
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Que desea hacer?',
+        choices: [
+        {
+            value: '1',
+            name: `${'1.'.green} Crear tarea`
+        },
+        {
+            value: '2',
+            name: `${'2.'.green} Listar tareas`
+        },
+        {
+            value: '3',
+            name: `${'3.'.green} Listar tareas completadas`
+        },
+        {
+            value: '4',
+            name: `${'4.'.green} Listar tareas pendientes`
+        },
+        {
+            value: '5',
+            name: `${'5.'.green} Completar tarea(s)`
+        },
+        {
+            value: '6',
+            name: `${'6.'.green} Borrar tarea`
+        },
+        {
+            value: '0',
+            name: `${'0.'.green} Salir`
+        }
+    ]
+    }
+]
+
+//Funcion para mostrar el menu
+const inquirerMenu = async() =>{
+
+    console.clear();
+    console.log('============================='.green);
+    console.log('   Seleccione una opcion'.white);
+    console.log('============================= \n'.green);
+
+    //Se muestran las opciones
+    const {opcion} = await inquirer.prompt(preguntas);
+
+    //Obtenemos la opcion seleccionada
+    return opcion;
+}
+
+//Funcion para pausar la aplicacion
+//Se pausa hasta que el usuario presione enter
+const pausa = async() => {
+
+    //Pregunta para pausar
+    const question = [{
+        type: 'input',
+        name: 'enter',
+        message: `Presione ${'Enter'.green} para continuar`
+    }];
+
+    console.log('\n');
+    
+    //Se espera a que el usuario presione enter
+    const {enter} = await inquirer.prompt(question);
+    
+    //Obtenemos el enter
+    return enter;
+
+}
+
+
+//Funcion para leer un input
+const leerInput = async (message)=>{
+
+    //Pregunta para leer un input
+    const question = [
+        //Se espera un input
+        {
+        type: 'input',
+        name: 'desc',
+        message,
+        validate(value){
+            if(value.length === 0){
+                return 'Por favor ingrese un valor';
+            }
+            return true;
+        }
+        
+    }
+];
+
+    const {desc} = await inquirer.prompt(question);
+    return desc;
+};
+
+//Funcion para listar las tareas a borrar
+const listadoTareasBorrar = async( tareas = [] )=>{
+
+    //Se evalua si el listado de tareas esta vacio
+    //Se usa el metodo map para obtener un nuevo arreglo con las tareas
+    const choices = tareas.map((tarea, i) =>{
+
+        //Se obtiene el indice de la tarea
+        const idx =  `${i + 1}.`.green;
+        //Se retorna el valor y el nombre de la tarea
+        return {
+            value: tarea.id,
+            name: `${idx} ${tarea.desc}`
+        }
+    })
+
+    //Se agrega la opcion de cancelar
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    });
+
+    //Se hace la lista de las tareas a borrar
+    const preguntas = [
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Borrar',
+            choices
+        }
+    ]
+    //Se obtiene el id de la tarea a borrar
+    const {id} = await inquirer.prompt(preguntas);
+
+    //Se retorna el id de la tarea a borrar
+    return id;
+
+}
+
+const confirmar = async( message )=>{
+
+    //Pregunta para confirmar
+    const question = {
+
+        //Se espera un confirm (Yes/No)
+        type: 'confirm',
+        name: 'ok',
+        message
+    };
+
+    //Se obtiene la respuesta
+    const {ok} = await inquirer.prompt(question);
+    return ok;
+}
+
+const mostrarListadoChecklist = async( tareas = [] )=>{
+
+    //Se evalua si el listado de tareas esta vacio
+    //Se usa el metodo map para obtener un nuevo arreglo con las tareas
+    const choices = tareas.map((tarea, i) =>{
+
+        //Se obtiene el indice de la tarea
+        const idx =  `${i + 1}.`.green;
+        //Se retorna el valor y el nombre de la tarea
+        return {
+            value: tarea.id,
+            name: `${idx} ${tarea.desc}`,
+            checked: (tarea.completadoEn)? true : false
+        }
+    })
+
+    //Se hace la lista de las tareas a borrar
+    const pregunta = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Seleccione',
+            choices
+        }
+    ]
+    //Se obtiene el id de la tarea a borrar
+    const {ids} = await inquirer.prompt(pregunta);
+
+    //Se retorna el id de la tarea a borrar
+    return ids;
+
+}
+
+module.exports ={
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist,
+};
