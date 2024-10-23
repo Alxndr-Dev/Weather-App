49f508f59ec3a1dabdc1a36d8751aec44319c7fb
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-23 00:15:52 -0400

    Adding Menu

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const { leerInput } = require("./helpers/inquirer")
+
+
+const {
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require("./helpers/inquirer");
 
 
 
-const main = async()=>{
+const main = async () => {
 
-    const texto = await leerInput('Hola: ');
+    let opt = '';
 
-    console.log(texto);
+    do{
+
+        opt = await inquirerMenu();
+        
+        console.log({opt});
+
+        switch (opt) {
+            case 1:
+                console.log('Buscar ciudad');
+                break;
+        
+            case 2:
+                console.log('Historial');
+                break;
+
+            case 0:
+                console.log('Salir');
+                break;
+        }
+
+        if ( opt !== 0 ) await pausa();
+
+    } while ( opt !==0 );
 }
 
 main();
