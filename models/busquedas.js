4b4340be41e812709b621b6bf05403b1493a2643
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-24 23:11:52 -0400

    App control model

---
+++ models/busquedas.js
@@ -0,0 +1,25 @@
+
+
+
+class Busquedas {
+
+    historial = ['Madrid', 'Bogota', 'San Jose', 'Buenos Aires', 'Caracas'];
+
+    constructor(){
+        //TODO: leer DB si existe
+    }
+
+
+    async ciudad( lugar = ''){
+
+        //Peticion HTTP
+        console.log(lugar);
+
+
+        return []; //Return the places that match the search
+    }
+
+}
+
+
+module.exports = Busquedas;
